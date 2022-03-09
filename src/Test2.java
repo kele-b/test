9c08da2235d9af83a7d12272e80851c3b33173e5
Bojan Kelecevic <b.kelecevic97@gmail.com>
2022-03-09 10:57:19 +0100

    Dodavanje nove klase za git Test2

---
+++ src/Test2.java
@@ -0,0 +1,9 @@
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Drugi testni prgram");
+        for(int i = 1; i<=10; i++){
+            System.out.println(i);
+        }
+        System.out.println("Ispis brojeva od 1 do 10");
+    }
+}
