c887e99ec4cc311507c4dedb31df2e2eafb6f34a
Bojan Kelecevic <b.kelecevic97@gmail.com>
2022-03-09 11:11:49 +0100

    Dodavanje nove grane na git

---
+++ src/Test3NovaGrana.java
@@ -0,0 +1,10 @@
+public class Test3NovaGrana {
+    public static void main(String[] args) {
+        int i =1;
+        while (i<=10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Ispis brojeva od 1 do 10 while petljom");
+    }
+}
